4c78235693b5dc67c1214786cfff3ad43e97ae70
Tevin Mukudi <deskcomp10@outlook.com>
2024-10-29 15:01:13 -0400

    UI without serial

---
+++ code/main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+from impact_screens import screen_manager
+
+root = tk.Tk()
+root.geometry("800x480")
+
+# screen image holder
+screen_holder = tk.Label(root)
+screen_holder.pack()
+
+# create screen manager
+manager = screen_manager(root, screen_holder)
+
+root.after(100, manager.handle_screens)
+# set bindings
+root.bind("<Button-1>", manager.handle_press)
+
+# set bindings
+root.mainloop()
