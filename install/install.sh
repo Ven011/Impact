d09502360af64e50a012e2ba2a0ac40c60c4e021
Tevin Mukudi <deskcomp10@outlook.com>
2024-12-13 14:37:57 -0500

    remove functools install

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,15 @@
-cp Impact.service /etc/systemd/system
-systemctl daemon-reload
-systemctl enable Impact.service
+# Update the raspberry pi
+sudo apt-get update 
+sudo apt-get upgrade -y
+
+# Install software packages
+sudo apt-get install -y python3-tk
+pip3 install pyserial
+pip3 install pillow
+#pip3 install functools
+
+
+sudo cp Impact.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable Impact.service
 
